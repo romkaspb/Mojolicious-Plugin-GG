8420b7f39a3133b500d97958105fbb7c64b2ac7e
romkaspb <rnovoselov93@gmail.com>
2017-01-25 17:35:37 +0000

    dev dev

--- lib/Mojolicious/Plugin/Admin.pm
+++ lib/Mojolicious/Plugin/Admin.pm
@@ -5,14 +5,23 @@ use Mojo::File 'path';
 
 our $VERSION = '0.01';
 
+has panel => sub { Mojolicious::Plugin::Admin::Panel->new(shift) };
+
 sub register {
-  my ($self, $app) = @_;
+  my ($self, $app, $config) = @_;
   # INIT
 
   # Append "templates" and "public" directories
   my $base = path(__FILE__)->dirname->child('Admin');
   push @{$app->renderer->paths}, $base->child('templates')->to_string;
   push @{$app->static->paths},   $base->child('public')->to_string;
+  $app->defaults(layout => 'admin');
+
+  $config = undef unless keys %$config;
+  die $config;
+  $config ||= $app->plugin('Config', { file => 'admin.conf' }) || die __PACKAGE__." no config defined";
+
+  $app->config->{admin} = $config;
 
   my $r = $app->routes;
   # generate routes first
@@ -32,14 +41,62 @@ sub register {
   });
 
   my $admin = $r->under('/admin')->to(cb => sub {
+    my $self = shift;
+
     # check auth
     return 1;
   });
   $admin->get('/')->to(cb => sub {
     shift->render('index');
     });
-  # $admin->get('/dashboard')->to
 
+  $admin->get('/dashboard')->to(cb => sub {
+
+  });
+
+  $self->panel( $config );
+
+
+  $app->helper( 'admin.generate_routes' => sub {
+    my $self = shift;
+    my $config = shift;
+
+    my $routes_names_array = _build_routes_array( $config->{controllers}, undef );
+    die $self->dumper( $config );
+    die $self->dumper( $routes_names_array );
+  });
+
+
+  $app->hook( before_dispatch => sub { my $c = shift; $c->admin->generate_routes; } );
+}
+
+sub _build_routes_array{
+  my ( $self, $controllers, $routes, $current ) = @_;
+  $routes   ||= [];
+  $current  ||= [];
+
+  if ( scalar @$current ){
+    my $ckey = $current->[ scalar @$current ];
+
+    push @$current, $ckey;
+
+    if( $controllers->{ $ckey }->{nested} ){
+      _build_routes_array( $controllers->{ $ckey }, $routes, $current );
+    }else{
+      push @$routes, $current;
+    }
+  }
+
+  foreach my $ckey ( keys %$controllers ){
+    push @$current, $ckey;
+    if( $controllers->{ $ckey }->{nested} ){
+      _build_routes_array( $controllers->{ $ckey }, $routes, $current );
+    }else{
+      push @$routes, $current;
+    }
+  }
+
+  return $routes;
 }
 
 1;
